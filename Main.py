09576831d426cb63320955231b6694a37fe6ba20
MUY SENGLY <muy.sengly@itc.edu.kh>
2025-09-16 23:54:42 +0700

    update

--- Main.py
+++ Main.py
@@ -4,10 +4,6 @@
 import os
 
 
-os.environ["QT_SCALE_FACTOR"] = "1"  # Set scaling factor
-os.environ["QT_AUTO_SCREEN_SCALE_FACTOR"] = "1"  # Enable automatic scaling
-os.environ["QT_SCREEN_SCALE_FACTORS"] = "1"  # Set screen scaling
-
 os.chdir("./resource/view/main_attendance_form/")
 
 os.system("python Controller.py")
